a37f6b573fb9c9ed97ffa6e13f22747df0f2b820
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-04-21 18:50:53 -0300

    Organizando rotas em pasta única (routes).

--- api/index.js
+++ api/index.js
@@ -1,13 +1,9 @@
 const express = require("express")
-const bodyParser = require("body-parser")
+const routes = require ("./routes/index.js")
 const app = express()
 const port = 3000
 
-app.use(bodyParser.json())
-app.get('/test', (req, res) => { res
-    .status(200)
-    .send({ mensagem: 'boas-vindas à API'})
-})
+routes(app)
 app.listen(port, () => console.log(` servidor rodando em ${port} `))
 
 module.exports = app 
