d5ccc2107d0a7a9090cad56568a9616878f80ed1
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-04-23 18:19:48 -0300

    Associando e referenciando tabelas.

--- api/models/matriculas.js
+++ api/models/matriculas.js
@@ -4,13 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Matriculas extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Matriculas.belongsTo(models.Pessoas, {
+        foreignKey: "estudante_id"
+      })
+      Matriculas.belongsTo(models.Turmas, {
+        foreignKey: "turma_id"
+      })
     }
   }
   Matriculas.init({
